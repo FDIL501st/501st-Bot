41aa3b001e293b129abffe3b4360cb35f4673414
FDIL <fdil.fadilh1@gmail.com>
2025-07-02 02:12:03 +0000

    Add another fix to conversation listener.
    Hopefully listener won't block other commands.

--- bot/cogs/gpt4all/conversation.py
+++ bot/cogs/gpt4all/conversation.py
@@ -43,37 +43,26 @@ class Conversation(commands.Cog):
         self.conversationBot: ConversationBot = ConversationBot(gpt_model)
 
 
-    @commands.Cog.listener()
-    async def on_message(self, message: nextcord.Message):
+    @commands.Cog.listener('on_message')
+    async def handle_listener(self, message: nextcord.Message):
         print("Conversation listener enter.")
         try:
-            await self.handle_listener(message)
+            # Any message sent to just you(hidden messages) don't have a guild association
+            # aka emphemeral message
+
+            if message.guild is None:
+                return # do nothing as was unable to find guild from message
+            
+            # can remove this if later if wanted, for now only occur within club server
+            # also only for messages not by the bot (don't want bot endlessly talking to itself)
+            # and for now only in the club server bot channel
+            # and ignore command prefixes
+            if message.guild.id == CLUB_SERVER_ID and message.author.id != BOT_ID and message.channel.id == CLUB_SERVER_BOT_CHANNEL and not message.content.startswith('./'):            
+                # for now need to use model to reply to message
+                reply = await self.conversationBot.reply(message.content)
+                await message.channel.send(reply)
         except:
             print("Conversation listener error.")
-
-    async def handle_listener(self, message: nextcord.Message):
-        # Any message sent to just you(hidden messages) don't have a guild association
-        # aka emphemeral message
-
-        if message.guild is None:
-            print("No guild found")
-            return # do nothing as was unable to find guild from message
-        
-        # can remove this if later if wanted, for now only occur within club server
-        # also only for messages not by the bot (don't want bot endlessly talking to itself)
-        # and for now only in the club server bot channel
-        # and ignore command prefixes
-        if message.guild.id == CLUB_SERVER_ID and message.author.id != BOT_ID and message.channel.id == CLUB_SERVER_BOT_CHANNEL and not message.content.startswith('./'):            
-            # for now need to use model to reply to message
-
-            # note: message.reply is a shortcut to ctx.send, what it does is get the context of the message (include channel)
-            # and call send to it
-
-            # debug messasge, check wat application id is
-            reply = await self.conversationBot.reply(message.content)
-            await message.channel.send(reply)
-
-
 
 
 def setup(bot):
